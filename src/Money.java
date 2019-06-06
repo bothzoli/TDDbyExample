ea3946e9f29c4c7e1fa0d2944d102400b0d80482
Zoltan Both <zoltan.both@sap.com>
2019-06-06 08:07:09 +0200

    Times We're Livin' In

--- src/Money.java
+++ src/Money.java
@@ -1,16 +1,26 @@
 abstract public class Money {
 
     protected int amount;
+    protected String currency;
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     abstract Money times(int multiplier);
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
 
     @Override
     public boolean equals(Object object) {
