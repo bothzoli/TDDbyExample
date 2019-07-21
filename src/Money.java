de388a4f3df82e24854e466f2d968b12f6d72da8
bothzoli <bothzoli@gmail.com>
2019-07-21 10:48:36 +0200

    The Root of All Evil

--- src/Money.java
+++ src/Money.java
@@ -4,11 +4,11 @@ public class Money {
     protected String currency;
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public Money times(int multiplier) {
